3697c8f7e07e7fde520456cfa9ad7fc26e83fcfc
ragulasrinivas555 <ragulasrinivas555@gmail.com>
2022-06-30 20:09:29 +0530

    added a new function

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,2 +1,9 @@
 dataprocessing.py
-print("creating machine model for data processing)
+print("creating machine model for data processing")
+
+a=5
+b=7
+sum(a,b):
+    c=a+b
+    return a
+print('added new function sum which is not correct')
